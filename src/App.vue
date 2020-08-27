5ce1363c822bf0de1e5a68169ce0e73f93d73d0b
Brian <brian.habib@brandnlabel.com>
2020-08-26 22:01:21 -0400

    Corrigiendo logica para intervalos con variable del estado

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,37 @@
 <template>
-  <div id="app">
-    <h1>{{contadorMin < 10 ? '0'+contadorMin : contadorMin }}:{{contadorSeg < 10 ? '0'+contadorSeg : contadorSeg }} min/seg</h1>
-    <div class="botones">
-      <button @click="iniciar(3)">3 segs</button>
-      <button @click="iniciar">1 min</button>
-      <button @click="iniciar">2 min</button>
-    </div>
+<div id="app">
+  <h1>{{contadorMin < 10 ? '0'+contadorMin : contadorMin }}:{{contadorSeg < 10 ? '0'+contadorSeg : contadorSeg }} min/seg</h1>
+  <div class="botones">
+    <button @click="iniciar(3)">3 segs</button>
+    <button @click="iniciar(60)">1 min</button>
+    <button @click="iniciar">2 min</button>
   </div>
+</div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
-import Navbar from "./components/Navbar.vue";
-
 export default {
   name: "Goku",
-  components: {
-    Navbar,
-  },
+
   data() {
     return {
       contadorMin: 0,
-      contadorSeg: 0
-
-    }
-      
+      contadorSeg: 0,
+      myIntervalo: null,
+    };
   },
-  // function myFunction() {
-  // setInterval(function(){ alert("Hello"); }, 3000);
+
   methods: {
     iniciar(time) {
-      this.contadorSeg = time
-      setInterval(()=>{this.contadorSeg > 0 ? this.contadorSeg-- : false},1000)
+      this.contadorSeg = time;
+      clearInterval(this.myIntervalo);
+      this.myIntervalo = setInterval(() => {
+        this.contadorSeg > 0 ? this.contadorSeg-- : false;
+        console.log("Soy el intervalo >:-)");
+      }, 1000);
+      setTimeout(() => {
+        clearInterval(this.myIntervalo);
+      }, (time + 1) * 1000);
     },
   },
 };
