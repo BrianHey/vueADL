19a79d7d6954fed693705bb7a8f41e221905ffb6
Viviana <vivi_reyes_c@hotmail.com>
2020-08-31 20:51:48 -0300

    primer componente

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  
+<div>
+  <Character />
+</div>
+
 </template>
 
 <script>
+import Character from "./components/Character";
 export default {
-
-}
+  components: {
+    Character,
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/Character.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <img src="https://static.vix.com/es/sites/default/files/styles/1x1/public/imj/p/paisajes-espectaculares-del-mundo-1.jpg" alt="">
+      <h3>El hermoso sur de Chile</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Character'
+
+}
+</script>
+
+<style>
+
+</style>
