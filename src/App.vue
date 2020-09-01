d3a4686ccc0b88404480991ee95b0f94b627dcbb
Wladimir Guevara <wladige@gmail.com>
2020-08-31 20:18:32 -0400

    agregando al estado src y mssg a character

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <Character />
+  <Character :src="src" :mssg="mssg"/>
 </div>
 
 </template>
@@ -10,6 +10,12 @@ import Character from "./components/Character";
 export default {
   components: {
     Character,
+  },
+  data() {
+    return {
+      src: "https://static.vix.com/es/sites/default/files/styles/1x1/public/imj/p/paisajes-espectaculares-del-mundo-1.jpg",
+      mssg: "El hermoso sur de Chile"
+    }
   },
 };
 </script>
--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-      <img src="https://static.vix.com/es/sites/default/files/styles/1x1/public/imj/p/paisajes-espectaculares-del-mundo-1.jpg" alt="">
-      <h3>El hermoso sur de Chile</h3>
+      <img :src="src" alt="">
+      <h3>{{mssg}}</h3>
+    
   </div>
 </template>
 
 <script>
-export default {
-    name: 'Character'
 
+export default {
+    name: 'Character',
+    props:{
+      src: String,
+      mssg: String
+    }
 }
 </script>
 
