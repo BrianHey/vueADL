9601a9f5594cd4c8152cce9296a12b71081c89ff
Brian <brian.habib@brandnlabel.com>
2020-09-02 19:25:30 -0400

    reset clase miercoles ay papa

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,5 @@
 <template>
 <div>
-  <button @click="saludar">clickeame po! Soy parte del Padre</button>
-  <div v-for="(p, i) in personajes" :key="i">
-    <Character @toctoc="saludar" :src="p.picture" :name="p.nombre" />
-  </div>
+  <h1>papa</h1>
 </div>
 </template>
-
-<script>
-import Character from "./components/Character";
-export default {
-  components: {
-    Character,
-  },
-  data() {
-    return {
-      personajes: [],
-    };
-  },
-  methods: {
-    saludar(indice) {
-      this.personajes.splice(indice, 1);
-    },
-  },
-  mounted() {
-    fetch("https://rickandmortyapi.com/api/character")
-      .then((response) => response.json())
-      .then((json) => {
-        let data = json.results;
-
-        data.slice(0, 5).forEach((el) => {
-          let nombre = el.name;
-          let picture = el.image;
-
-          this.personajes.push({
-            nombre,
-            picture,
-          });
-        });
-      })
-      .catch((err) => {
-        console.log(`todo esta perdido rick ${err}`);
-      });
-  },
-};
-</script>
-
-<style>
-</style>
--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,24 +1,3 @@
 <template>
-<div>
-  <img :src="src" alt />
-  <h3 @click="avisaleApapa">{{name}}</h3>
-</div>
+<div>hijito</div>
 </template>
-
-<script>
-export default {
-  name: "Character",
-  props: {
-    src: String,
-    name: String,
-  },
-  methods: {
-    avisaleApapa() {
-      this.$emit("toctoc", 2);
-    },
-  },
-};
-</script>
-
-<style>
-</style>
