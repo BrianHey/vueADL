f8e5111a935c78318b0480bac5466706e3e67ddf
Brian <brian.habib@brandnlabel.com>
2020-09-02 19:56:16 -0400

    hooks

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <template>
 <div>
   <h1>papa</h1>
+  <input type="text" v-model="name">
 </div>
 </template>
 
 <script>
 export default {
   name: "app",
+  data() {
+    return {
+      name: ''
+    }
+  },
+  beforeCreate() {
+    console.log("Hola soy 1");
+  },
+  created() {
+    console.log("Hola soy 2");
+  },
+  beforeMount() {
+    console.log("Hola soy 3");
+  },
+  mounted() {
+    console.log("Hola soy 4");
+  },
+  beforeUpdate() {
+      console.log("Hola soy beforeUpdate");
+      console.log(this.name)
+  },
+  updated() {
+    console.log("Hola soy updated");
+      console.log(this.name)
+    
+  },
 };
 </script>
