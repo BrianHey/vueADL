753aaa9df95f94218e5a47bed5da7f382aed6418
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2020-09-02 21:04:24 -0400

    obtener nombre, imagen y movimientos de cada pokemon desde la API

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,53 @@
 <template>
-	<div>
-		<input v-model="nombre" type="text" />
-		<!--  <button @click="buscar">Pokémon</button>
- <div>
-    <img :src="imagen" alt="">
-    <h6>{{nombrePokemon}}</h6>
-    <p>{{movimientos}}</p>
-  </div> -->
-	</div>
+  <div>
+    <input v-model="nombre" type="text" @keyup.enter="buscarPokemon" />
+    <button @click="buscarPokemon">Pokémon</button>
+    <div>
+      <img :src="imagen" alt />
+      <h6>{{nombrePokemon}}</h6>
+      <p v-for="(movimiento, i) in movimientos" :key="i">{{movimiento.move.name}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'app',
-	data() {
-		return {
-			nombre: ' ',
-			pokemon: {},
-		};
-	},
-	created() {
-		this.buscarPokemon();
-	},
-	methods: {
-		buscarPokemon() {
-			fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-				.then(function(response) {
-					return response.json();
-				})
-				.then((myJson) => {
+  name: "app",
+  data() {
+    return {
+      nombre: " ",
+      pokemon: {
+        name: "",
+        sprites: { front_default: "" },
+        moves: [],
+      },
+    };
+  },
+  created() {
+    this.buscarPokemon();
+  },
+  methods: {
+    buscarPokemon() {
+      fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+        .then(function (response) {
+          return response.json();
+        })
+        .then((myJson) => {
           console.log(myJson);
-           this.pokemon = myJson
+          this.pokemon = myJson;
         });
-		},
-	},
+    },
+  },
+  computed: {
+    imagen() {
+      return this.pokemon.sprites.front_default;
+    },
+    nombrePokemon() {
+      return this.pokemon.name;
+    },
+    movimientos() {
+      return this.pokemon.moves;
+    },
+  },
 };
 </script>
