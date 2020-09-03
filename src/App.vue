e9a9dec4a1ceabef949561dc2bccc63741595776
Viviana <vivi_reyes_c@hotmail.com>
2020-09-02 21:12:47 -0300

    template del primer desafio

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
 <div>
-  <h1>papa</h1>
+  <input v-model="pikachu" type="text"> 
+  <button @click="buscar">Pokémon</button>
+  <div>
+    <img :src="imagen" alt="">
+    <h6>{{nombre}}</h6>
+    <p>{{movimientos}}</p>
+  </div>
 </div>
 </template>
 
